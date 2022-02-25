7b959f267f2b6af79bcd0e9c59280c0837cd4553
Ramadan <kudlacag114@yahoo.com>
2022-02-25 13:40:07 +0100

    an ather commit

--- userData.js
+++ userData.js
@@ -5,10 +5,5 @@ export const weight = document.getElementById('weight');
 export const diet = document.getElementById('diet');
 
 
-export const divTiles = document.createElement('div');
-divTiles.classList.add('grid-item');
-export const image = document.createElement('img');
-export const p = document.createElement('p');
-export const h3 = document.createElement('h3');
-export const  fragment = document.createDocumentFragment();
+
 
